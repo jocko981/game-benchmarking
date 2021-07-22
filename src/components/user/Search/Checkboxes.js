66e0ed4f9bb035693e4738c0ca42be83bf9627cb
jocko981 <jovannikolic17@gmail.com>
2021-07-22 15:58:51 +0200

    Search page, left to fix Sorting functions

---
+++ src/components/user/Search/Checkboxes.js
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from "react";
+
+const Checkboxes = ({ games }) => {
+    const inputs = ['Won Award', 'Single Player', 'Violence'];
+    const types = ['MOBA', 'FPS', 'MMORPG'];
+    const sorts = ['Rating', 'Players'];
+
+    const [checkedInputs, setCheckedInputs] = useState(new Array(inputs.length).fill(false));
+    const [checkedTypes, setCheckedTypes] = useState(new Array(types.length).fill(false));
+    const [checkedSorts, setCheckedSorts] = useState(new Array(sorts.length).fill(false));
+    const [filterGames, setFilterGames] = useState(games);
+
+    console.log(filterGames, 'filterGames - state')
+
+    const handleWonAward = (arr) => {
+        if (checkedInputs[0]) {
+            return arr.filter(item => item.won_award.toString() === '1');
+        } else {
+            return arr;
+        }
+    }
+    const handleSinglePlayer = (arr) => {
+        if (checkedInputs[1]) {
+            return arr.filter(item => item.single_player.toString() === '1');
+        } else {
+            return arr;
+        }
+    }
+    const handleViolence = (arr) => {
+        if (checkedInputs[2]) {
+            return arr.filter(item => item.violence.toString() === '1');
+        } else {
+            return arr;
+        }
+    }
+    const handleType = (arr) => {
+        if (checkedTypes[0]) {
+            return arr.filter(item => item.type === 'MOBA');
+        } else if (checkedTypes[1]) {
+            return arr.filter(item => item.type === 'FPS');
+        } else if (checkedTypes[2]) {
+            return arr.filter(item => item.type === 'MMORPG');
+        } else {
+            return arr;
+        }
+    }
+    const handleRatingSort = (arr) => {
+        if (checkedSorts[0]) {
+            return arr.filter(item => item.won_award.toString() === '1');
+        } else {
+            return arr;
+        }
+    }
+    const handlePlayersSort = (arr) => {
+        if (checkedSorts[1]) {
+            return arr.sort((a,b) => a.num_of_players_favourite - b.num_of_players_favourite);
+        } else {
+            return arr;
+        }
+    }
+    // .sort((a, b) => b.num_of_players_favourite - a.num_of_players_favourite).map(item => {
+    //     return <div key={item.ID}>{item.name} - {item.num_of_players_favourite}</div>
+
+    useEffect(() => {
+        //Filter options updated so apply all filters here
+        let result = games;
+        result = handleSinglePlayer(result);
+        result = handleWonAward(result);
+        result = handleViolence(result);
+        result = handleType(result);
+        result = handlePlayersSort(result);
+        // result = filtersearchName(result);
+        setFilterGames(result);
+    }, [checkedInputs, checkedTypes, checkedSorts]);
+
+    const handleInputs = (position) => {
+        const updateCheckedState = checkedInputs.map((item, index) => index === position ? !item : item);
+        setCheckedInputs(updateCheckedState);
+    }
+    const handleTypes = (position) => {
+        const updateCheckedState = checkedTypes.map((item, index) => index === position ? !item : false);
+        setCheckedTypes(updateCheckedState);
+    }
+    const handleSorts = (position) => {
+        const updateCheckedState = checkedSorts.map((item, index) => index === position ? !item : false);
+        setCheckedSorts(updateCheckedState);
+    }
+
+    return (
+        <>
+            <div className="checkboxes_wrapper">
+                <div>
+                    {inputs.map((item, index) => {
+                        return (
+                            <div className="checkbox_div" key={index}>
+                                <input type="checkbox"
+                                    checked={checkedInputs[index]}
+                                    name={item}
+                                    value={item}
+                                    id={`input-${index}`}
+                                    onChange={() => handleInputs(index)}
+                                />
+                                <label htmlFor={`input-${index}`}>{item}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+
+                <div>
+                    <label>Filter by type:</label>
+                    {types.map((item, index) => {
+                        return (
+                            <div className="checkbox_div" key={index}>
+                                <input type="checkbox"
+                                    checked={checkedTypes[index]}
+                                    name={item}
+                                    value={item}
+                                    id={`type-${index}`}
+                                    onChange={() => handleTypes(index)}
+                                />
+                                <label htmlFor={`type-${index}`}>{item}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div>
+                    <label>Sort by:</label>
+                    {sorts.map((item, index) => {
+                        return (
+                            <div className="checkbox_div" key={index}>
+                                <input type="checkbox"
+                                    checked={checkedSorts[index]}
+                                    name={item}
+                                    value={item}
+                                    id={`sort-${index}`}
+                                    onChange={() => handleSorts(index)}
+                                />
+                                <label htmlFor={`sort-${index}`}>{item}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+
+            <div className="filtered_games">
+                <div className="ui celled list">
+
+                    {filterGames.map((item, index) => {
+                        return (
+                            <div className="item" key={item.ID}>
+                                <div className="header">
+                                    {item.name}
+                                </div>
+                                <div className="description">
+                                    <div>Rating: {item.rating}</div>
+                                    <div>Global players: {item.num_of_players_global}</div>
+                                </div>
+                            </div>
+                        );
+                    })}
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Checkboxes;
--- src/components/user/Search/Search.js
+++ src/components/user/Search/Search.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { fetchAllGames } from "../../../actions";
+import Checkboxes from "./Checkboxes";
+import "./Search.css";
 
-const Search = () => {
+const Search = (props) => {
+    useEffect(() => {
+        fetchAllGames();
+    }, [fetchAllGames])
 
     return (
         <div className="content-page-wrapper">
             <h1>Search</h1>
+
+            <div className="search_wrapper">
+
+                <div className="input_div ui input">
+                    <input type="text" placeholder="Search for games..." />
+                </div>
+
+                <Checkboxes games={props.games} />
+
+            </div>
         </div>
     );
 }
 
-export default Search;
+const mapStateToProps = (state) => {
+    return {
+        games: Object.values(state.games)
+    };
+}
+
+export default connect(mapStateToProps, { fetchAllGames })(Search);
