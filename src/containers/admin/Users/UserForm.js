2ce15c737547e54d36fa2478e046dd98784057e8
jocko981 <jovannikolic17@gmail.com>
2021-08-09 14:58:17 +0200

    Making userForm validation (error & warn) within inputs & dropdown

--- src/containers/admin/Users/UserForm.js
+++ src/containers/admin/Users/UserForm.js
@@ -5,7 +5,8 @@ import { Field, formValues, reduxForm } from "redux-form";
 class UserForm extends React.Component {
     // const { handleSubmit } = this.props;    -Ovo je iz redux-forme, i ima ga u <form>
 
-    renderError({ error, touched }) {
+    // for redux-form
+    renderErrorTouched({ error, touched }) {
         if (touched && error) {
             return (
                 <div className="ui error message">
@@ -14,9 +15,37 @@ class UserForm extends React.Component {
             );
         }
     }
+    renderError({ error, submitFailed }) {
+          console.log(submitFailed, 'E submitFailed')
+
+        if (submitFailed && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            );
+        }
+    }
+    renderWarn({ warning, submitFailed }) {
+        if (submitFailed) {
+            return (
+                <div className="ui yellow compact message">
+                    <div className="header">
+                        <i className="warning icon" />
+                        {warning}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    // rendering functions
     renderInput = ({ input, label, meta, type }) => { // (field)  je ovde input, a izvukli smo {input,label,meta,type}
         // console.log(input, 'input', label, 'label', meta, 'meta')
         const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+        //           ${meta.error && meta.submitFailed}  - to render error only after submitting empty input(s)
 
         return (
             <div className={className}>
@@ -26,7 +55,23 @@ class UserForm extends React.Component {
             </div>
         );
     }
-    // ideja !  - napravi 2 comp za 2 inputa
+    renderDropdown = ({ input, meta, label }) => {
+        //   console.log('dropDown - { meta }', meta)
+        const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <select {...input}>
+                    <option />
+                    <option value="ff0000">Red</option>
+                    <option value="00ff00">Green</option>
+                    <option value="0000ff">Blue</option>
+                </select>
+                <div>{this.renderWarn(meta)}</div>
+            </div>
+        );
+    }
 
     onSubmit = (formValues) => {
         this.props.onSubmit(formValues);
@@ -40,12 +85,15 @@ class UserForm extends React.Component {
                 <Field name="name" type="text" component={this.renderInput} label="Enter: name" />
                 <Field name="password" type="text" component={this.renderInput} label="Enter: password" />
 
+                <Field name="favoriteColor" component={this.renderDropdown} label="Select a color: " />
+
                 <div className="field">
                     <label>Select role: </label>
 
                     <div className="field">
                         <div className="ui radio checkbox">
-                            <Field name="role" type="radio" component="input" value="user" id="user" props={{ value: "user" }} />
+                            <Field name="role" type="radio" component="input" value="user" id="user" />
+                            {/* props={{ value: "user" }}   - inside <Field> */}
                             <label htmlFor="user">user</label>
                         </div>
                     </div>
@@ -58,6 +106,13 @@ class UserForm extends React.Component {
 
                     {/* <Field name="role" type="radio" component={this.renderInputRadio} /> */}
                 </div>
+                <div className="ui warning message">
+  <i className="close icon"></i>
+  <div className="header">
+    You must register before you can do that!
+  </div>
+  Visit our registration page, then try again
+</div>
 
                 <button className="ui button primary">Submit</button>
             </form>
@@ -65,6 +120,7 @@ class UserForm extends React.Component {
     }
 }
 
+// for redux-form
 const validate = (formValues) => {
     const errors = {};
     const msg = "You must enter this.";
@@ -80,22 +136,26 @@ const validate = (formValues) => {
     }
     // if (!formValues.role) {
     //     errors.role = msg;
-    // }
-    // ovo je conditional, admin accout ima 'role' a kod obicnog usera ne postoji 'role' keyvalue uopste
+    // }   
+    //     radio btn: user/admin role - default is 'user'
+    if (!formValues.favoriteColor) {
+        errors.favoriteColor = "Where is your color??";
+    }
     return errors;
 }
 
-// const warn = values => {
-//     const warnings = {}
-//     if (values.age < 19) {
-//         warnings.age = 'Hmm, you seem a bit young...'
-//     }
-//     return warnings
-// } // we can also add warning
+const warn = (values) => {
+    const warnings = {}
+
+    if (!values.favoriteColor) {
+        warnings.favoriteColor = 'Hmm come on, tell us your favorite color...';
+    }
+    return warnings;
+} // we can also add warning
 
 export default reduxForm({
     form: "userForm",
     initialValues: { role: "user" },
     validate: validate,
-    // warn : ward // <--- warning function given to redux-form
+    warn // <--- warning function given to redux-form
 })(UserForm);
