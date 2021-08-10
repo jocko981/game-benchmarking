d0b88a5398f9db17303f219de92fffbaa77b493a
jocko981 <jovannikolic17@gmail.com>
2021-08-10 09:42:31 +0200

    adding prev page history & icon for 404 page

--- src/components/errorPages/ErrorPage404.js
+++ src/components/errorPages/ErrorPage404.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import history from "../../history";
 import "./ErrorPage404.css";
 
-const ErrorPage404 = (props) => {
+const ErrorPage404 = () => {
 
     return (
         <div className="wrapper_404">
             <div className="middle aligned column">
+
                 <h1>404 page not found</h1>
-                <div onClick={() => props.history.push("/login")} className="ui big button">
+
+                <div onClick={() => history.go(-1)} className="ui big button">
+                    <i className="arrow alternate circle left icon"></i>
+                    Go back to Previous page
+                </div>
+
+                <div onClick={() => history.push("/login")} className="ui big button">
                     <i className="signup icon"></i>
                     Go back to Login page
                 </div>
+
             </div>
         </div>
     );
